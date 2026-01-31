6af81496b6f65498ac87fddde2c79f1c5f0d67b4
ibbe479 <ibrahim.halawani04@gmail.com>
2026-01-31 05:17:14 +0100

    f

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from supabase import create_client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # läser .env
+
+url = os.getenv("SUPABASE_URL")
+key = os.getenv("SUPABASE_KEY")
+
 def ansluta_till_db():
     try:
         supabase = create_client(url, key)
@@ -8,7 +17,7 @@ def ansluta_till_db():
 
 def hamta_data(supabase):
     try:
-        response = supabase.table("planets").select("*").execute()
+        response = supabase.table("Users").select("*").execute()
         print(response.data)
     except Exception as e:
         print("Fel vid hämtning:", e)
@@ -20,4 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-fe
